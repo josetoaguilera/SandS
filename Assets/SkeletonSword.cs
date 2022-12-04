9daabff5153bab525f86634e2faa2270ee221b20
José Aguilera <joseaguilera@Mac-mini-de-Jose.local>
2022-12-04 01:08:37 -0300

    se agrega el inicio de los créditos y el healthbar

--- Assets/SkeletonSword.cs
+++ Assets/SkeletonSword.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class SkeletonSword : MonoBehaviour
 {
-    public float daño = 2;
+    public float Hitpoints;
+    public float MaxHitpoints = 2;
+    public HealthbarBehaviour Healthbar;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Hitpoints = MaxHitpoints;
+        Healthbar.SetHealth(Hitpoints, MaxHitpoints);
     }
 
     // Update is called once per frame
@@ -16,11 +20,20 @@ public class SkeletonSword : MonoBehaviour
     {
         
     }
+    public void TakeHit(float damage)
+    {
+        Hitpoints -= damage;
+
+        if (Hitpoints <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
 
     private void OnTriggerEnter(Collider other) {
         Debug.Log("aaa");
         if (other.gameObject.GetComponent<SistemaVidas>() != null) {
-            other.gameObject.GetComponent<SistemaVidas>().QuitarVida(daño);
+            other.gameObject.GetComponent<SistemaVidas>().QuitarVida(Hitpoints);
         }
     }
 }
